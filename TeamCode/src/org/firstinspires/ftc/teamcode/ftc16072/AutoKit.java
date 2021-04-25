917cbf1c501fdc7a1f4a625bede4041f8b26d835
diegoAaguilar <diegoaguilar454@yahoo.com>
2021-04-25 12:53:14 -0400

    EDUKit drive mode source code and the configurations/javafx to use the virtual robot with it also auto mode 0.0

--- TeamCode/src/org/firstinspires/ftc/teamcode/ftc16072/AutoKit.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/ftc16072/AutoKit.java
@@ -1,15 +1,16 @@
 package org.firstinspires.ftc.teamcode.ftc16072;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @Autonomous(name = "AutoKit", group = "FTCKitBot")
-public class AutoKit extends OpMode {
+public class AutoKit extends LinearOpMode {
     private DcMotor left = null;
     private DcMotor right = null;
     private DcMotor armY = null;
@@ -17,41 +18,78 @@ public class AutoKit extends OpMode {
     private RevColorSensorV3 colorSensor = null;
     private TouchSensor touchSensor = null;
 
-    boolean buttonState = false;
-    boolean done = false;
+    private boolean buttonState = false;
+    private boolean done = false;
 
-    int numPresses = 0;
-    int colorInd = 0; // 0 = green, 1 = yellow, 2 = red
+    private boolean lockedIn = false;
+    private int lastColorInd = -1;
+    private float lastColorIndTime = -1;
+    private float colorIndCounter = 0;
+    private float lockInTime = 15;
 
-    @Override
-    public void init() {
-        while (!super.isStarted()) {
-            left = hardwareMap.dcMotor.get("left_motor");
-            right = hardwareMap.dcMotor.get("right_motor");
-            left.setDirection(DcMotor.Direction.REVERSE);
-            armY = hardwareMap.dcMotor.get("arm_motor");
-            hand = hardwareMap.servo.get("hand_servo");
-            colorSensor = (RevColorSensorV3) hardwareMap.get("color_sensor");
-            touchSensor = hardwareMap.touchSensor.get("touch_sensor");
-            if (touchSensor.isPressed() ^ buttonState) {
-                buttonState = !buttonState;
-                if (buttonState) {
-                    numPresses++;
-                }
-            }
-            telemetry.addData("rgba: ","("+colorSensor.red()+", "+colorSensor.green()+", "+colorSensor.blue()+", "+colorSensor.alpha()+")");
-            telemetry.addData("g-a: ",Math.abs(colorSensor.green()-colorSensor.red()));
-            colorInd = (colorSensor.green()>colorSensor.red()) ? 0:2;
-            colorInd = (Math.abs(colorSensor.green()-colorSensor.red()) <= 0.05) ? 1:colorInd;
-            telemetry.addData("colorInd: ",colorInd);
-            telemetry.update();
-        }
-        armY.setPower(-0.1);
+    private int numPresses = 0;
+    private int colorInd = 0; // 0 = green, 1 = yellow, 2 = red
+
+    public void initialize() {
+        left = hardwareMap.dcMotor.get("left_motor");
+        right = hardwareMap.dcMotor.get("right_motor");
+        left.setDirection(DcMotor.Direction.REVERSE);
+        armY = hardwareMap.dcMotor.get("arm_motor");
+        hand = hardwareMap.servo.get("hand_servo");
+        colorSensor = (RevColorSensorV3) hardwareMap.get("color_sensor");
+        touchSensor = hardwareMap.touchSensor.get("touch_sensor");
     }
 
-    @Override
-    public void loop() {
-        if (done) return;
+    public void initialize_loop() {
+        hand.setPosition(0);
+        if (touchSensor.isPressed() ^ buttonState) {
+            buttonState = !buttonState;
+            if (buttonState) {
+                numPresses++;
+                lockedIn = false;
+            }
+        }
+        if (lockedIn) {
+            telemetry.addData("colorInd Locked:", true);
+            telemetry.addData("colorInd:", colorInd);
+            telemetry.update();
+            return;
+        }
+        NormalizedRGBA colors = colorSensor.getNormalizedColors();
+        int alpha = Range.clip((int)(colors.alpha * 256), 0, 255);
+        int red = Range.clip((int)(colors.red * 256), 0, 255);
+        int green = Range.clip((int)(colors.green * 256), 0, 255);
+        int blue = Range.clip((int)(colors.blue * 256), 0, 255);
+        telemetry.addData("rgba: ","("+red+", "+green+", "+blue+", "+alpha+")");
+        telemetry.addData("g-a: ",Math.abs(green-red));
+        colorInd = (green>red) ? 0:2;
+        colorInd = (Math.abs(green-red) <= 15) ? 1:colorInd;
+        telemetry.addData("colorInd: ",colorInd);
+        if (colorInd == lastColorInd) {
+            float deltaTime = (System.nanoTime()/(float) Math.pow(10,9))-lastColorIndTime;
+            colorIndCounter += deltaTime;
+            if (colorIndCounter >= lockInTime) {
+                lockedIn = true;
+            }
+        }
+        else if (lastColorInd == -1) {
+            lastColorIndTime = System.nanoTime()/(float) Math.pow(10,9);
+            lastColorInd = colorInd;
+        }
+        else {
+            colorIndCounter = 0;
+            lastColorInd = colorInd;
+        }
+        telemetry.addData("colorInd timer:", colorIndCounter);
+        telemetry.addData("colorInd Locked:", lockedIn);
+        telemetry.update();
+    }
+
+    public void finalLoop() {
+        if (done) {
+            return;
+        }
+        armY.setPower(0);
         float power;
         if (colorInd == 0) {
             power = 1;
@@ -66,49 +104,78 @@ public class AutoKit extends OpMode {
         if (power != 0) {
             for (int i = 0; i < numPresses; i++) {
                 int lastPos = armY.getCurrentPosition();
-                while (armY.getCurrentPosition() - lastPos < ticksPerRevHex / 8) {
-                    armY.setPower(0.7);
+                while (Math.abs(armY.getCurrentPosition() - lastPos) < ticksPerRevHex / 6f) {
+                    armY.setPower(-0.5);
+                    telemetry.addData("armPos Up: ",Math.abs(armY.getCurrentPosition() - lastPos));
+                    telemetry.addData("Must be greater than: ",ticksPerRevHex / 6f);
+                    telemetry.update();
                 }
                 lastPos = armY.getCurrentPosition();
-                while (armY.getCurrentPosition() - lastPos < ticksPerRevHex / 8) {
-                    armY.setPower(-0.1);
+                while (Math.abs(armY.getCurrentPosition() - lastPos) < ticksPerRevHex / 10f) {//because of how the motor has some give in that it wont turn if the arm only turns a little
+                    telemetry.addData("armPos Down: ",Math.abs(armY.getCurrentPosition() - lastPos));
+                    telemetry.addData("Must be greater than: ",ticksPerRevHex / 10f);
+                    telemetry.update();
+                    armY.setPower(0);
                 }
             }
+            float startTime = (float) (System.nanoTime()/Math.pow(10,9));
+            float lastTime = startTime;
+            while (Math.abs(startTime-lastTime) < numPresses) {
+                lastTime = (float) (System.nanoTime()/Math.pow(10,9));
+                telemetry.addData("Time: ", Math.abs(startTime-lastTime));
+                telemetry.addData("Time to wait: ", numPresses);
+                telemetry.update();
+            }
         }
-
-        float startTime = (float) (System.nanoTime()/Math.pow(10,9));
-        float lastTime = startTime;
-        while (lastTime-startTime < numPresses) {
-            lastTime = (float) (System.nanoTime()/Math.pow(10,9));
-        }
-
-        while (power == 0) {
-            telemetry.addData("rgba: ","("+colorSensor.red()+", "+colorSensor.green()+", "+colorSensor.blue()+", "+colorSensor.alpha()+")");
-            telemetry.addData("g-a: ",Math.abs(colorSensor.green()-colorSensor.red()));
-            colorInd = (colorSensor.green()>colorSensor.red()) ? 0:2;
-            colorInd = (Math.abs(colorSensor.green()-colorSensor.red()) <= 0.05) ? 1:colorInd;
-            telemetry.addData("colorInd: ",colorInd);
-            power = (new float[] {1,0.5f,0})[colorInd];
-            telemetry.update();
+        else {
+            while (power < 1) {
+                telemetry.addData("rgba: ","("+colorSensor.red()+", "+colorSensor.green()+", "+colorSensor.blue()+", "+colorSensor.alpha()+")");
+                telemetry.addData("g-a: ",Math.abs(colorSensor.green()-colorSensor.red()));
+                colorInd = (colorSensor.green()>colorSensor.red()) ? 0:2;
+                colorInd = (Math.abs(colorSensor.green()-colorSensor.red()) <= 0.05) ? 1:colorInd;
+                telemetry.addData("colorInd: ",colorInd);
+                power = (new float[] {1,0.5f,0})[colorInd];
+                telemetry.update();
+            }
+            return;
         }
         float circumference = 0.295276f*((float) Math.PI);
         float numRevsFor3Ft = 3f/circumference;
-        while (true) {
-            int lastPos = armY.getCurrentPosition();
-            while (armY.getCurrentPosition() - lastPos < ticksPerRevHex*numRevsFor3Ft) {
-                left.setPower(power);
-                right.setPower(power);
-            }
-            hand.setPosition(0.5);
-            float lastTime2 = startTime;
-            while (lastTime2-startTime < 0.5f) {
-                lastTime2 = (float) (System.nanoTime()/Math.pow(10,9));
-            }
-            while (armY.getCurrentPosition() - lastPos < ticksPerRevHex*numRevsFor3Ft) {
-                left.setPower(-power);
-                right.setPower(-power);
-            }
+        int lastPos = left.getCurrentPosition();
+        float ticksPerRev = (float) left.getMotorType().getTicksPerRev();
+        while (Math.abs(left.getCurrentPosition()) - lastPos < ticksPerRev*numRevsFor3Ft) {
+            left.setPower(-power);
+            right.setPower(-power);
         }
+        left.setPower(0);
+        right.setPower(0);
+        hand.setPosition(0.5);
+        //wait
+        float startTime2 = (float) (System.nanoTime()/Math.pow(10,9));
+        float lastTime2 = startTime2;
+        while (Math.abs(startTime2-lastTime2) < 1) {
+            lastTime2 = (float) (System.nanoTime()/Math.pow(10,9));
+        }
+        //go back
+        lastPos = left.getCurrentPosition();
+        while (Math.abs(left.getCurrentPosition() - lastPos) < ticksPerRev*numRevsFor3Ft) {
+            left.setPower(power);
+            right.setPower(power);
+        }
+        left.setPower(0);
+        right.setPower(0);
         done = true;
+    }
+
+    public void runOpMode() {
+        initialize();
+        while (!super.isStarted()) {
+            initialize_loop();
+        }
+        while (!done) {
+            finalLoop();
+        }
+        telemetry.addData("Auto status: ", "Done");
+        telemetry.update();
     }
 }
